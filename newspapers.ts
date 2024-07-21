b0aecaecdd52e66b7f361dc79c6e0eeafbbed56b
Steven Madow <steven@madow.com>
2024-07-21 19:00:22 -0400

    Update newspapers.ts

--- newspapers.ts
+++ newspapers.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import fs from 'fs';
-import im from 'imagemagick';
 import path from 'path';
 import pLimit from 'p-limit';
+import { exec } from 'child_process';
 
 import { logger } from './logger';
 import config from './config.json';
@@ -11,22 +11,21 @@ import newspapers from './newspapers.json';
 export const NEWSPAPER_BASE_URL = 'https://cdn.freedomforum.org/dfp/pdf{DAY_OF_MONTH}/';
 export const NEWSPAPAPER_CACHE_PATH = path.resolve(__dirname, './newspaper-cache');
 
-export const downloadFile = async (url: string, path: string) => {
+export const downloadFile = async (url: string, filePath: string) => {
   logger(`Downloading ${url}`);
   try {
     const response = await axios(url, {
       responseType: 'arraybuffer',
     });
-    await fs.writeFileSync(path, response.data);
+    await fs.writeFileSync(filePath, response.data);
   } catch (error: any) {
-    // gracefully fail if a file fails to download
     logger(`Failed: ${error.message}`, { sentiment: 'negative', processLevel: 2 });
   }
 };
 
 export const getDirectoryContents = async (dir: string): Promise<string[]> => {
   return new Promise((resolve, reject) => {
-    fs.readdir(dir, (err: NodeJS.ErrnoException | null, files: string[] | undefined) => {
+    fs.readdir(dir, (err, files) => {
       if (err) {
         console.error('Error reading directory:', err);
         reject(err);
@@ -37,7 +36,7 @@ export const getDirectoryContents = async (dir: string): Promise<string[]> => {
         reject('No files found');
         return;
       }
-      const filteredFiles = files.filter((file: string) => file !== 'jpgs' && !file.startsWith('.'));
+      const filteredFiles = files.filter(file => file !== 'jpgs' && !file.startsWith('.'));
       console.log('Filtered Files:', filteredFiles);
       resolve(filteredFiles);
     });
@@ -75,67 +74,21 @@ export const downloadNewspaperPDFs = async () => {
   await Promise.all(papersToDownload);
 };
 
-export const getImageProperties = async (file: string): Promise<im.Features> => {
+export const convertPdfToImage = async (inputFile: string, outputDir: string) => {
   return new Promise((resolve, reject) => {
-    im.identify(file, async (err, features) => {
-      if (err) reject(err);
-      resolve(features);
-    });
-  });
-};
-
-export const convertImage = async (inputFile: string, outputFile: string) => {
-  return new Promise((resolve, reject) => {
-    im.convert(
-      ['-density', '225', '-background', 'White', '-layers', 'flatten', inputFile, outputFile],
-      async err => {
-        if (err) reject(err);
-        resolve(true);
+    exec(`python3 pdf_to_image.py "${inputFile}" "${outputDir}"`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error converting PDF to image: ${error.message}`);
+        reject(error);
+        return;
       }
-    );
-  });
-};
-
-export const cropImage = async (file: string, width: number, height: number) => {
-  return new Promise((resolve, reject) => {
-    im.convert(['-gravity', 'North', '-crop', `${width}x${height}+0+0`, file, file], async err => {
-      if (err) reject(err);
       resolve(true);
     });
   });
 };
 
-export const extendImage = async (file: string, width: number, height: number) => {
-  return new Promise((resolve, reject) => {
-    im.convert(['-extent', `${width}x${height}`, file, file], async err => {
-      if (err) reject(err);
-      resolve(true);
-    });
-  });
-};
-
-export const processPdf = async (inputFile: string, convertedFileName: string) => {
+export const processPdf = async (inputFile: string, outputDir: string) => {
   logger(`Converting ${inputFile}`);
-  await convertImage(inputFile, convertedFileName);
-
-  const desiredAspectRatio = 16 / 9;
-  const threshholdForRatioEnforcement = 0.02; // meural will crop the image which is probably fine, but not beyond 2%
-
-  const imageProperties = await getImageProperties(convertedFileName);
-
-  const imageWidth = imageProperties.width || 0;
-  const imageHeight = imageProperties.height || 0;
-
-  const percentageOverOrUnderRatio = 1 - (imageWidth * desiredAspectRatio) / imageHeight;
-
-  if (Math.abs(percentageOverOrUnderRatio) > threshholdForRatioEnforcement) {
-    if (percentageOverOrUnderRatio < 0) {
-      logger(`Extending ${convertedFileName}`);
-      await extendImage(convertedFileName, imageWidth, imageWidth * desiredAspectRatio);
-    } else {
-      logger(`Cropping ${convertedFileName}`);
-      await cropImage(convertedFileName, imageWidth, imageWidth * desiredAspectRatio);
-    }
-  }
-  logger(`Processed ${convertedFileName}`, { sentiment: 'positive' });
+  await convertPdfToImage(inputFile, outputDir);
+  logger(`Processed ${inputFile}`, { sentiment: 'positive' });
 };
